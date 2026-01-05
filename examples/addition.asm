57bbca6cb271f7b5819c02aca9e54e3e3cba8a33
krolxon <krolyxon@tutanota.com>
2026-01-05 12:17:04 +0530

    add LICENSE

--- examples/addition.asm
+++ examples/addition.asm
@@ -1,4 +1,8 @@
+; move imm 10 to register A
 mov a, 10
+; move imm 5 to register B
 mov b, 5
+; Add a & B
 add a, b
+; Halt
 hlt
--- examples/loops.asm
+++ examples/loops.asm
@@ -1,8 +1,13 @@
+; move imm 3 to register b
 mov b, 3
+; move imm 1 to register a
 mov a, 1
 
+; delcare a label
 loop:
+    ; subtract a from b
     sub b, a
-    jz loop
+    ; jump to label "loop" until the zero flag is not set
+    jnz loop
 
 hlt
